11ffb30c34bd53b7b2e54bd55765e1ed72c2d43e
MarioneOg <ogboimarione@gmail.com>
2025-04-09 21:01:22 -0500

    Fixed naming + import errors in the program

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask
+from flask import Flaskpip 
+from flask import Flask 
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import.JWTManager
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app(config_object="Config.DevelopmentConfig"):
-	app = Flask(_name_)
+	app = Flask(__name__)
 	app.config.from_object(config_object)
 	
 	CORS(app)
 	db.init_app(app)
 	jwt.init_app(app)
 	
-	from routes.tasks import tasks_bp
-	from routes.users import users_bp
+	from RoutesTask import tasks_bp
+	from RoutesUsers import users_bp
 
 	app.register_blueprint(tasks_bp, url_prefix='/api/tasks')
 	app.register_blueprint(users_bp, url_prefix='/api/users')
@@ -25,6 +26,6 @@ def create_app(config_object="Config.DevelopmentConfig"):
 
 	return app
 
-if _name_ == "_main_":
+if __name__ == "_main_":
 	app = create_app()
 	app.run(debug=True)
