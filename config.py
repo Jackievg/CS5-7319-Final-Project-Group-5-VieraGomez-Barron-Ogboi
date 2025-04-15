82b98ab5ebee1806b86f6aedc16a11bb6cd61a2b
MarioneOg <ogboimarione@gmail.com>
2025-04-14 20:14:06 -0500

    adding a timedelta to the configuration so the token does not expire so quickly

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-replace-in-production')
@@ -9,6 +10,8 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///task_management.db'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
+
 
 class ProductionConfig(Config):
     DEBUG = False
